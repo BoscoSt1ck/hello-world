9bd389fd87f7fd0c11bbfb0acd6731a61679b8d1
Bosco Nguyen <boscotn1234@gmail.com>
2025-09-18 15:41:19 -0400

    New print line
    added new print line.

--- HelloWorld1.java
+++ HelloWorld1.java
@@ -3,5 +3,6 @@ public class HelloWorld1
    public static void main(String[] args)
    {
       system.out.print("Hello World");
+      system.out.print("Program modified, but able to revert to original version using GitHub Desktop");
    }
 }
